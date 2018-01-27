7d47e13fccbe286f34a28163e6c6a59ed1057d7d
SamPratik <pratik.anwar@gmail.com>
2018-01-28 01:47:08 +0600

    Tutorial 34 - Params Observable

--- src/app/employee-detail.component.ts
+++ src/app/employee-detail.component.ts
@@ -1,18 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { EmployeeService } from './employee.service';
 
 @Component({
   template: `
               <h2>Employee Details</h2>
               <p>The id of the employee is id = {{ employeeId }}</p>
+              <a (click)="goNext()">Next</a>
+              <a (click)="goPrevious()">Previuos</a>
             `
 })
 
 export class EmployeeDetailComponent {
   employeeId: number;
-  constructor(private router: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
   ngOnInit() {
-    this.employeeId = this.router.snapshot.params['id'];
+    this.route.params.subscribe((params: Params) => {
+      let id = parseInt(params['id']);
+      this.employeeId = id;
+    });
+  }
+
+  goNext() {
+    if(this.employeeId == 5) {
+      this.employeeId = 1;
+      this.router.navigate(['/details', 1]);
+    } else {
+      this.router.navigate(['/details', this.employeeId + 1]);
+    }
+  }
+
+  goPrevious() {
+    if(this.employeeId == 1) {
+      this.employeeId = 5;
+      this.router.navigate(['/details', 5]);
+    } else {
+      this.router.navigate(['/details', this.employeeId - 1]);
+    }
   }
 }
