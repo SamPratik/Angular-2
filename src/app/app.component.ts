5918d9b400d5afeda632aa35d9a2857f9a364522
SamPratik <pratik.anwar@gmail.com>
2018-01-29 02:51:57 +0600

    Tutorial - 39 - Animations in Angular 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  keyframes,
+  group
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
               <h2>Root Component</h2>
-              <router-outlet></router-outlet>
+              <button (click)="toggle()">Toggle</button>
+              <div class="room" [@lightsOnOff]="roomState"></div>
             `,
+  styles: [
+    `
+      .room {
+        width: 200px;
+        height: 200px;
+        background-color: black;
+        border: 1px solid black;
+      }
+    `
+  ],
+  animations: [
+    trigger('lightsOnOff', [
+      state('off', style({
+        backgroundColor: 'black'
+      })),
+      state('on', style({
+        backgroundColor: 'white'
+      })),
+      transition('on => off', [animate('2s')]),
+      transition('off => on', [animate('2s')])
+    ])
+  ]
 })
 export class AppComponent {
-
+  roomState: string = 'off';
+  toggle() {
+    this.roomState = (this.roomState === 'off') ? 'on' : 'off';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
-import { routingComponents } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import {TutorialsComponent} from './tutorials.component';
-import { EmployeeListComponent } from './employee-list.component';
-import { EmployeeDetailComponent } from './employee-detail.component';
-import { PageNotFoundComponent } from './page-not-found.component';
 
 import { EmployeeService } from './employee.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TutorialsComponent,
-    routingComponents,
-    PageNotFoundComponent
+    TutorialsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ReactiveFormsModule,
-    AppRoutingModule
+    ReactiveFormsModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
