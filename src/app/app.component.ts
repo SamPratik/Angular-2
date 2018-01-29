3278ce17e3c964474e810ff88af3fa40321862b9
SamPratik <pratik.anwar@gmail.com>
2018-01-29 16:48:42 +0600

    Tutorial 44 - Parallel Animation Groups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,17 +14,9 @@ import {
   template: `
               <h2>Root Component</h2>
               <button (click)="toggle()">Toggle</button>
-              <button (click)="toggleHeight()">Toggle Height</button>
-              <div class="room text-center" [@lightsOnOff]="roomState">
+
+              <div class="room text-center" [@groupTrigger]="roomState">
                 <strong>Rotate 90 degree</strong>
-              </div><br>
-              <div class="heightBox" [@heightTrigger]="heightState" [@lightsOnOff]="roomState">
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
               </div>
             `,
   styles: [
@@ -35,44 +27,39 @@ import {
         background-color: black;
         border: 1px solid black;
       }
-      .heightBox {
-        border: 1px solid black;
-        width: 200px;
-      }
     `
   ],
   animations: [
-    trigger('lightsOnOff', [
+    trigger('groupTrigger', [
       state('off', style({
-        backgroundColor: 'black',
-        color: 'white'
+        backgroundColor: 'black'
       })),
       state('on', style({
         backgroundColor: 'white'
       })),
-      transition('on => off', [animate('2s 3s ease-in', style({transform: 'rotate(90deg)'}))]),
-      transition('off => on', [animate('2s ease-out', style({transform: 'rotate(-90deg)'}))])
-    ]),
-    trigger('heightTrigger', [
-      state('noHeight', style({
-        height: '0px'
-      })),
-      state('fullHeight', style({
-        height: '*'
-      })),
-      transition('noHeight <=> fullHeight', animate('2s'))
+      transition('off => on', [group([
+        animate('4s ease-in', style({
+          transform: 'rotate(90deg)'
+        })),
+        animate('2s ease-out', style({
+          width: '50px'
+        }))
+      ])]),
+      transition('on => off', [group([
+        animate('3s 1s ease-in', style({
+          transform: 'rotate(360deg)'
+        })),
+        animate('4s', style({
+          opacity: '0'
+        }))
+      ])])
     ])
   ]
 })
 export class AppComponent {
   roomState: string = 'off';
-  heightState: string = 'fullHeight';
 
   toggle() {
     this.roomState = (this.roomState === 'off') ? 'on' : 'off';
-  }
-
-  toggleHeight() {
-    this.heightState = (this.heightState === 'fullHeight') ? 'noHeight' : 'fullHeight';
   }
 }
